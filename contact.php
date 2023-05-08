1551f32df52eb61b8c0ea9487527d1f982f1312e
GeorgeKaraiskos <129230246+GeorgeKaraiskos@users.noreply.github.com>
2023-05-08 04:48:27 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,47 +4,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Prepare the data to be sent
-    $data = [
-        "name" => $name,
-        "email" => $email,
-        "message" => $message
-    ];
+    // Configure your email settings
+    $to = "geokaraisk513@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n" .
+               "Reply-To: $email\r\n" .
+               "X-Mailer: PHP/" . phpversion();
 
-    // Convert data to JSON format
-    $jsonData = json_encode($data);
+    // Compose the email content
+    $emailContent = "Name: $name\n" .
+                    "Email: $email\n" .
+                    "Message: $message";
 
-    // Set the Formspree endpoint
-    $formspreeEndpoint = "https://formspree.io/f/xoqzrbyb";
-
-    // Set the HTTP headers
-    $headers = [
-        "Content-Type: application/json"
-    ];
-
-    // Create a new cURL resource
-    $curl = curl_init();
-
-    // Set the cURL options
-    curl_setopt_array($curl, [
-        CURLOPT_URL => $formspreeEndpoint,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $jsonData,
-        CURLOPT_HTTPHEADER => $headers
-    ]);
-
-    // Execute the cURL request
-    $response = curl_exec($curl);
-
-    // Close the cURL resource
-    curl_close($curl);
-
-    // Check if the request was successful
-    if ($response === "OK") {
-        echo "Message sent successfully!";
+    // Send the email
+    if (mail($to, $subject, $emailContent, $headers)) {
+        echo "Message sent successfully.";
     } else {
-        echo "An error occurred while sending the message.";
+        echo "Error sending the message.";
     }
 }
 ?>
