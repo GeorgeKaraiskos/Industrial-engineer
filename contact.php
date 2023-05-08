76289323dcbad4229d97e0821f66794dd991d08a
GeorgeKaraiskos <129230246+GeorgeKaraiskos@users.noreply.github.com>
2023-05-08 03:52:48 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,23 +1,27 @@
-﻿<?php
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Configure your email settings
-    $to = "geokaraisk513@gmail.com";
-    $subject = "New Contact Form Submission";
-    $headers = "From: $email\r\n" .
-               "Reply-To: $email\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+    // Formspree endpoint
+    $formspreeEndpoint = "https://formspree.io/f/xoqzrbyb";
 
     // Compose the email content
     $emailContent = "Name: $name\n" .
                     "Email: $email\n" .
                     "Message: $message";
 
-    // Send the email
-    if (mail($to, $subject, $emailContent, $headers)) {
+    // Send the email using Formspree endpoint
+    $ch = curl_init($formspreeEndpoint);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $emailContent);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Check if the email was sent successfully
+    if ($response === "OK") {
         echo "Message sent successfully.";
     } else {
         echo "Error sending the message.";
